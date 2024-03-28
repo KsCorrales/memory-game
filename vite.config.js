d991f5520e1e2a1155b9a01d542537bc29459834
Kscorrales <kscorrales@hotmail.com>
2024-03-28 17:57:51 -0500

    chore: vite.config.js base update.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://kscorrales.github.io/memory-game",
   plugins: [
     vue(),
   ],
