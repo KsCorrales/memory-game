7cc7fbad27f3d539f579f3e404042562acba0ac4
Kscorrales <kscorrales@hotmail.com>
2024-03-29 00:37:33 -0500

    test: improved test cases for game store

--- test/store/game.spec.ts
+++ test/store/game.spec.ts
@@ -9,26 +9,33 @@ describe('Game store actions', () => {
   })
 
   it('restartGame should leave default values again, for prevent old stored values on restart games', () => {
-    const store = useGame()
-    store.restartGame()
-    expect(store.pairedCards).toBe([])
-    expect(store.revealing).toBe(false)
-    expect(store.lastFlippedCard).toBe(0)
+    const gameStore = useGame();
+
+    gameStore.pairedCards = [1, 2, 10];
+    gameStore.revealing = true;
+    gameStore.lastFlippedCard = 10;
+
+    gameStore.restartGame();
+
+    expect(gameStore.pairedCards).toEqual([]);
+    expect(gameStore.revealing).toBe(false);
+    expect(gameStore.lastFlippedCard).toBe(0);
   });
 
   it('addPairedCard should push a card number to the paired cards store', () => {
-    const store = useGame()
-    expect(store.pairedCards).toBe([])
-    store.addPairedCard(5)
-    expect(store.pairedCards).toBe([5])
+    const gameStore = useGame();
+
+    gameStore.addPairedCard(5);
+
+    expect(gameStore.pairedCards).toEqual([5]);
   });
 
   it('reveal should set to on revealing store and set it to off after 1 second', async () => {
-    const store = useGame()
-    expect(store.revealing).toBe(false)
-    store.reveal()
-    expect(store.revealing).toBe(true)
+    const gameStore = useGame()
+    expect(gameStore.revealing).toBe(false)
+    gameStore.reveal()
+    expect(gameStore.revealing).toBe(true)
     await new Promise(resolve => setTimeout(resolve, 1001))
-    expect(store.revealing).toBe(false)
+    expect(gameStore.revealing).toBe(false)
   });
 })
