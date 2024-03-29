27f0e0977609c484212d91e76f53730a44f09f3a
Kscorrales <kscorrales@hotmail.com>
2024-03-29 10:29:32 -0500

    update title on index

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://kscorrales.github.io/memory-game' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://kscorrales.github.io/memory-game/' : './',
   plugins: [
     vue(),
   ],
